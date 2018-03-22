f3f73404f493fd393f6a01bcd1c2fe8e1a0dd27f
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2018-03-22 12:28:29 +0100

    setup theme name & screenshot, added favicon

--- website/wp-content/themes/t4a/header.php
+++ website/wp-content/themes/t4a/header.php
@@ -35,8 +35,14 @@
 		<?php wp_head(); ?>
 		<?php // end of wordpress head ?>
 
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+		<?php // icon stuff ?>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f7d180">
+        <meta name="msapplication-TileColor" content="#f7d180">
+        <meta name="theme-color" content="#f7d180">
 
 	</head>
 
