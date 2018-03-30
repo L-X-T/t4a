47b37a79485f0e10b054acdfcf68ebd84329dce0
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2018-03-30 15:35:18 +0200

    updated menu styles

--- website/wp-content/themes/t4a/library/js/scripts.js
+++ website/wp-content/themes/t4a/library/js/scripts.js
@@ -157,7 +157,7 @@ function closeOffcanvasNav() {
 
     $('li.menu-toggle a').click(function(e) {
         $this = $(this);
-        $this.parent().toggleClass('opened');
+        $this.parent().toggleClass('toggled');
     });
 
 })(jQuery);
