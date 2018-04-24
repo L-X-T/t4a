0f25453199b5d50897369b6d20273d986bfd7255
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2018-04-24 13:14:17 +0200

    updated custom posts skin, added fancybox 3 plugin

--- website/wp-content/themes/t4a/elementor/skin-custom.php
+++ website/wp-content/themes/t4a/elementor/skin-custom.php
@@ -22,5 +22,64 @@ class Skin_Custom extends Skin_Base {
 		return __( 'T4A', 'elementor-pro' );
 	}
 
+	protected function render_post() {
+		$this->render_post_header();
+		$this->render_thumbnail();
+		$this->render_text_header();
+		$this->render_meta_data();
+		$this->render_title();
+		$this->render_excerpt();
+		$this->render_read_more();
+		$this->render_text_footer();
+		$this->render_post_footer();
+	}
+
+	protected function render_thumbnail() {
+		$thumbnail = $this->get_instance_value( 'thumbnail' );
+
+		if ( 'none' === $thumbnail && ! Plugin::elementor()->editor->is_edit_mode() ) {
+			return;
+		}
+
+		$settings = $this->parent->get_settings();
+		$setting_key = $this->get_control_id( 'thumbnail_size' );
+		$settings[ $setting_key ] = [
+			'id' => get_post_thumbnail_id(),
+		];
+		$thumbnail_html = Group_Control_Image_Size::get_attachment_image_html( $settings, $setting_key );
+
+		if ( empty( $thumbnail_html ) ) {
+			return;
+		}
+		?>
+		<a class="elementor-post__thumbnail__link" href="<?php echo get_permalink(); ?>">
+			<div class="elementor-post__thumbnail"><?php echo $thumbnail_html; ?></div>
+		</a>
+        <?php global $post; // var_dump(get_field('gallery'));
+        if (!empty(get_field('gallery')) && is_array(get_field('gallery'))) : ?>
+        <div class="gallery">
+            <?php foreach (get_field('gallery') as $index => $image) : ?>
+            <a href="<?php echo $image['url']; ?>" data-fancybox="gallery" data-caption="<?php echo $image['title']; ?>">
+                <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" />
+            </a>
+            <?php endforeach; ?>
+        </div>
+		<?php endif;
+	}
+
+	protected function render_title() {
+		if ( ! $this->get_instance_value( 'show_title' ) ) {
+			return;
+		}
+
+		$tag = $this->get_instance_value( 'title_tag' );
+		?>
+		<<?php echo $tag; ?> class="elementor-post__title">
+		<a href="<?php echo get_permalink(); ?>">
+			<?php the_title(); ?>
+		</a>
+		</<?php echo $tag; ?>>
+		<?php
+	}
 
 }
