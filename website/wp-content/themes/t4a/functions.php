9c04363d74e9538b6465a80ad054052ba1dfbdc5
Christian Ferzola <christian@ankerlos.at>
2018-05-03 20:31:40 +0200

    add menu walker

--- website/wp-content/themes/t4a/functions.php
+++ website/wp-content/themes/t4a/functions.php
@@ -8,6 +8,39 @@ just edit things like thumbnail sizes, header images,
 sidebars, comments, etc.
 */
 
+/************
+ADD WALKER
+************/
+class t4a_Walker extends Walker_Nav_Menu {
+// Displays start of an element. E.g '<li> Item Name'
+  // @see Walker::start_el()
+  function start_el(&$output, $item, $depth=0, $args=array(), $id = 0) {
+    $object = $item->object;
+    $type = $item->type;
+    $title = $item->title;
+    $description = $item->description;
+    $permalink = $item->url;
+    $output .= "<li class='" .  implode(" ", $item->classes) . "'>";
+
+    //Add SPAN if no Permalink
+    if( $permalink ) {
+      $output .= '<a href="' . $permalink . '">';
+    } else {
+      $output .= '<span>';
+    }
+
+    $output .= $title;
+    if( $description != '' ) {
+      $output .= '<small class="description">' . $description . '</small>';
+    }
+    if( $permalink ) {
+      $output .= '</a>';
+    } else {
+      $output .= '</span>';
+    }
+  }
+}
+
 // LOAD BONES CORE (if you remove this, the theme will break)
 require_once( 'library/bones.php' );
 
